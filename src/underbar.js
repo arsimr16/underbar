d5184e23d6331968aa3644a252b3b799b2741013
arsimr16 <arsimr16@g.holycross.edu>
2017-07-21 13:41:45 -0400

    get all tests to pass for underscore reject function

--- src/underbar.js
+++ src/underbar.js
@@ -96,6 +96,15 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+
+    _.filter(collection, function(item) {
+      if(!test(item)) {
+        result.push(item);
+      }
+    });
+
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
